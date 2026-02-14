878fd247a6e00eb2179f12fd896aeeafc6b44354
league <league@192.168.1.85>
2026-02-14 11:28:45 -0800

    gfhghdfghdfgh

---
+++ src/something/Platypus.java
@@ -0,0 +1,13 @@
+package something;
+
+public class Platypus {
+	  private String name;
+	  
+	  Platypus(String name){
+		  this.name = name;
+	  }
+	  
+	  void sayHi(){
+	        System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	  }
+}
---
+++ src/something/PlatypusRunner.java
@@ -0,0 +1,10 @@
+package something;
+
+public class PlatypusRunner {
+
+	public static void main(String[] args) {
+		Platypus thing1 = new Platypus("bob");
+		thing1.sayHi();
+	}
+
+}
