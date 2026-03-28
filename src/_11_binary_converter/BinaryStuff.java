120cd71de430071e8347382c064a650321866df7
league <league@192.168.1.75>
2026-03-27 18:24:13 -0700

    gdsfgdsg

--- src/_11_binary_converter/BinaryStuff.java
+++ src/_11_binary_converter/BinaryStuff.java
@@ -1,11 +1,13 @@
 package _11_binary_converter;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -17,20 +19,44 @@ public class BinaryStuff implements ActionListener {
 	JTextField joe = new JTextField(20);
 	void something() {
 		eli.addActionListener(this);
-		tim.add(jay);
-		tim.add(eli);
 		tim.add(joe);
+		eli.setText("convert");
+		tim.add(eli);
+		tim.add(jay);
 		bob.add(tim);
 		bob.pack();
 		bob.setVisible(true);
+		bob.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
 		new BinaryStuff().something();
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
-		
+		String something = convert(joe.getText());
+		jay.setText(something);
+		bob.pack();
 	}
+	
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
 
 }
